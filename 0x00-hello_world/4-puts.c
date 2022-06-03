ec245998739ce063edb2abae6e318e4c698de0f5
Bi-oluwatife <osunlajaboluwatife@gmail.com>
2022-06-02 18:18:52 -0700

    first code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * * main - prints the phrase "programming is like building a multilingual
- *  puzzle
- *  Return: 0 if exited properly, non-zero otherwise
+ *  main - Prints "programming is like building a multilingual
+ *  puzzle, followed by a new line"
+ *  Return:Always 0
  */
 int main(void)
 {
