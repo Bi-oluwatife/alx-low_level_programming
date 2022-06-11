523b278d950b4ce7bde6b1a5b09d1dd41347f600
Bi-oluwatife <osunlajaboluwatife@gmail.com>
2022-06-10 20:57:53 -0700

    positive/negative

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
 
+
+
 #define MAIN_H
 
 
@@ -9,6 +11,12 @@
 
 
 void positive_or_negative(int i);
+
+int largest_number(int a, int b, int c);
+
+int convert_day(int month, int day);
+
+void print_remaining_days(int month, int day, int year);
 
 
 
