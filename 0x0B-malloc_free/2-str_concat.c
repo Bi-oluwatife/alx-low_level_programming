e96adf70e98623a340dc7dad6e9fa97951990a61
boluwatife <osunlajaboluwatife@gmail.com>
2022-06-28 21:12:27 -0700

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,34 +1,43 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include "main.h"
 /**
- * _strdup - duplicates a string into newly allocated array
+ * str_concat - concatenates 2 strings
  *
- * @str: string to duplicate
+ * @s1: first string
+ * @s2: string to add to end of of first string
  *
- * Return: pointer to new string
+ * Return: pointer to newly allocated string concatenation
  */
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
-	int size = 0;
+	unsigned int size1 = 0, size2 = 0;
 	char *ptr, *ret;
 
-	if (!str)
-		return (NULL);
+	ptr = s1;
+	if (s1)
+		while (*ptr++)
+			size1++;
+	else
+		s1 = "";
 
-	ptr = str;
-	while (*ptr++)
-		size++;
+	ptr = s2;
+	if (s2)
+		while (*ptr++)
+			size2++;
+	else
+		s2 = "";
 
-	ret = malloc(size + 1);
+	ret = malloc(size1 + size2 + 1);
 	if (!ret)
 		return (NULL);
 
 	ptr = ret;
-	while (*str)
-		*ptr++ = *str++;
-
+	while (*s1)
+		*ptr++ = *s1++;
+	while (*s2)
+		*ptr++ = *s2++;
 	*ptr = 0;
+
 	return (ret);
 }
